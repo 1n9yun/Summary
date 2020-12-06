c9d712d69cba42f1e8fbcbd5f38425556a1c4a6b
1n9yun <jig7357@gmail.com>
2020-12-06 16:34:23 +0900

    201206

--- CS/DataStructure/codes/src/main/java/com/ds/example/implementations/LinkedList.java
+++ CS/DataStructure/codes/src/main/java/com/ds/example/implementations/LinkedList.java
@@ -5,11 +5,13 @@ import com.ds.example.interfaces.Deque;
 import com.ds.example.interfaces.List;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>, Deque<E>, Cloneable, Serializable {
-    int size = 0;
-    Node<E> first;
-    Node<E> last;
+    transient int size = 0;
+    transient Node<E> first;
+    transient Node<E> last;
 
     public LinkedList() {
 
@@ -19,7 +21,125 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
         addAll(c);
     }
 
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
 
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<E> descendingIterator() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E getLast() {
+        return null;
+    }
+
+    @Override
+    public E getFirst() {
+        return null;
+    }
+
+    @Override
+    public void addFirst(E e) {
+
+    }
+
+    @Override
+    public void addLast(E e) {
+
+    }
+
+    @Override
+    public boolean offerFirst(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean offerLast(E e) {
+        return false;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E peekFirst() {
+        return null;
+    }
+
+    @Override
+    public E peekLast() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E pollFirst() {
+        return null;
+    }
+
+    @Override
+    public E pollLast() {
+        return null;
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E removeFirst() {
+        return null;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        return false;
+    }
+
+    @Override
+    public E removeLast() {
+        return null;
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        return false;
+    }
 
     private static class Node<E> {
         E item;
@@ -31,5 +151,13 @@ public class LinkedList<E> extends AbstractSequentialList<E> implements List<E>,
             this.next = next;
             this.prev = prev;
         }
+    }
+
+    private class ListItr implements ListIterator<E> {
+        private Node<E> lastReturned;
+        private Node<E> next;
+        private int nextIndex;
+        private int expectedModCount = modCount;
+
     }
 }
--- CS/DataStructure/codes/src/main/java/com/ds/example/test.java
+++ CS/DataStructure/codes/src/main/java/com/ds/example/test.java
@@ -5,12 +5,10 @@ import java.util.*;
 public class test {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
-        Queue<Integer> q = new LinkedList<>();
         list.add(3);
         list.add(7);
         list.add(1);
         list.add(3);
         list.add(5);
-
     }
 }
